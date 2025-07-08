30961895e05b43e301e54b80dbb1dfccbc7ea843
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-08 09:54:13 +0000

    Operations in js

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,44 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// All these are arithmetic operations.
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Heyy"
+let str2 = " Manav"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 2) * 4 / 6 % 2);
+
+// Special type of conversion jisko tricky conversion bhi bolte hai.
+console.log(+true);
+console.log(+ "0");
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
