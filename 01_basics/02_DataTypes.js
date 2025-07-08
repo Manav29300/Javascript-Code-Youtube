f4475653214e4764fc8868dfcc1fa44b7afc3697
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-08 06:55:19 +0000

    DataTypes Notes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; 
+// This upper line meaning is they treat all JS Code as a newer version of Javascript.
+
+//alert( 3 + 3);
+//We are using Nodejs and this syntax is wrong according to NodeJS. We are not using browser this syntax is valid for browser.
+
+//In node we avoid semicolons.
+
+//String DataType
+let name = "Manav"
+
+// Number DataType
+let age = 23
+
+//Boolean DataType
+let isLoggedIn = false
+
+// DATA TYPES:-
+// PRIMITIVE DATA TYPES =>
+// range: number => 2 to power 53
+// bigInt
+// string => It is used in single and double cot both (("") (''))
+// boolean => True or False
+// null => ye ek standlone value hai.[Ye ek representation hai empty value ka.]
+// undefined => it means maine value define ni kri hai like( maine ek variable declare kra h 'example: let name;' 
+//              but maine isme value ni kri define to iska output hmara undefined ayega.)
+// Symbol => to find uniqueness symbol datatypes is used.
+
+// Object
+
+//typeof => ye kissi bhi datatype ka type pta krne ke liye use hota hai.
+
+console.log(typeof "Manav");
+console.log(typeof age);
+console.log(typeof null); // iska output null ayega.
+console.log(typeof undefined); // iska output undefined ayega.
+
