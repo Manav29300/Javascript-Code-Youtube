5d469a9d139f5b6b898d1de998bc7d01f8b9cd22
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-09 11:19:27 +0000

    Date and Time in js

---
+++ 01_basics/08_datesinjs.js
@@ -0,0 +1,24 @@
+// DATES
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate); 
+
+// let createmyDate = new Date(2023, 0, 23);
+// console.log(createmyDate.toDateString());
+
+let createmyDate = new Date("01-14-2023");
+console.log(createmyDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
+
+
