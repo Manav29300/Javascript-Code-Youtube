c32260ff6900ce9459f0e905c4b1f514e1d9d456
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-09 06:22:26 +0000

    Strings in JS

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -2,7 +2,7 @@ const name = "Manav"
 const repoCount = 50
 
 // This syntax is outdated.
-// console.log(name + repoCount + " Value");
+console.log(name + repoCount + " Value");
 
 // Updated Syntax
 // Backticks use krnege iska fayda hai yha pr aata h String Interpolation. Isse hum placeholders bnate hai unke andr jo bhi
@@ -18,6 +18,39 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+// .slice: agar hum isme negative values dete h to ye reverse se start hota hai.
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+// new String => (.trim) ye jo unknowlingly faltu spaces hoti h usko remove krta hai.
+const newStringone = "         Manav        "
+console.log(newStringone);
+console.log(newStringone.trim());
+
+//New String=> (.replace) ye replace krta h kissi bhi word ko url ya sentence mai se.
+const url = "https://manav.com//manav%20gaba";
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('welcome'));
+
+console.log(gameName.split("").join("\n"));
+
+let name1 = "ManavGaba";
+for(let i=0; i<name1.length; i++){
+    console.log(name1[i]);
+}
+
+
+
+
+
+
+
+
 
 
 
