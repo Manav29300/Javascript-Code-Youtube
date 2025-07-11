5f84cf3bf120a8a3a2276e0dbcf9a5a53b51a91c
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-11 06:28:43 +0000

    objects in js

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -30,7 +30,42 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// spread operator: 90% yhi syntax hoga use.
+const obj3 = {...obj1, ...obj2, ...obj4}
 console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "m@gmail.com"
+    },
+    {
+        id: 2,
+        email: "n@gmail.com"
+    },
+    {
+        id: 3,
+        email: "k@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+// ".entries " ye ek key value ko array mai bna deta hai.
+console.log(Object.entries(tinderUser));
+
+// objects ke andr purra loop through kr rhe h khi baar value exist ni krti to isse crash hone ke chances hote hai 
+// to iss method ke through simply hum usse puch skte h ke ye value exist krti h ya nhi.
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
 
 
