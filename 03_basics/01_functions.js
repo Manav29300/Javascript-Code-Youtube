5b5bdd314c4a0a5a023bc919998bc2c0845b86e4
Manav29300 <107296919+Manav29300@users.noreply.github.com>
2025-07-14 07:02:48 +0000

    Functions in Javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,13 +30,54 @@ const result = addTwoNumbers(3, 5)
 //     return `${username} just logged in`
 // }
 
-function loginUserMessage(username){
-    if (username === undefined){
+function loginUserMessage(username = "sam"){
+    // if (username === undefined){
+    //     console.log("Please enter Username");
+    // }
+    // return `${username} just logged in`
+
+    if (!username){
         console.log("Please enter Username");
+        return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Manav"));
-console.log(loginUserMessage());
+console.log(loginUserMessage("Manav"));
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 600, 2000))
+
+
+const user = {
+    username: "Manav",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 999
+})
+
+// iske andr arrays bhi pass kr skte hai hum.
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
 
